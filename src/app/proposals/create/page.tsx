dedb52724497af506cd560aa7f4e832df00c98ab
anarkrypto <anarkrypto@gmail.com>
2025-03-12 22:00:31 -0300

    feat: add metadata for Create Proposal page

--- src/app/proposals/create/page.tsx
+++ src/app/proposals/create/page.tsx
@@ -1,4 +1,9 @@
 import CreateProposalComponent from '@/components/CreateProposal'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+	title: 'Create Proposal | MEF',
+}
 
 export default function CreateProposalPage() {
 	return <CreateProposalComponent />
