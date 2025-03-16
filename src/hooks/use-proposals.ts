ef73987c61383e597abbe5d98a9e288a28cd9abb
anarkrypto <anarkrypto@gmail.com>
2025-03-16 19:16:55 -0300

    feat: update useProposals hook to support advanced query parameters and return proposal counts

--- src/hooks/use-proposals.ts
+++ src/hooks/use-proposals.ts
@@ -1,18 +1,27 @@
 'use client'
 
 import logger from '@/logging'
-import { ProposalSummaryWithUserAndFundingRound } from '@/types/proposals'
+import { GetProposalsOptionsSchema } from '@/schemas/proposals'
+import { ProposalsWithCounts } from '@/types/proposals'
 import { UndefinedInitialDataOptions, useQuery } from '@tanstack/react-query'
 
 export function useProposals(
+	{ query, filterBy, sortBy, sortOrder }: GetProposalsOptionsSchema = {},
 	options: Omit<
-		UndefinedInitialDataOptions<ProposalSummaryWithUserAndFundingRound[]>,
+		UndefinedInitialDataOptions<ProposalsWithCounts>,
 		'queryKey' | 'queryFn'
 	> = {},
 ) {
-	const url = '/api/proposals'
+	const searchParams = new URLSearchParams()
 
-	return useQuery<ProposalSummaryWithUserAndFundingRound[]>({
+	if (query) searchParams.set('query', query)
+	if (filterBy) searchParams.set('filterBy', filterBy)
+	if (sortBy) searchParams.set('sortBy', sortBy)
+	if (sortOrder) searchParams.set('sortOrder', sortOrder)
+
+	const url = `/api/proposals${searchParams.size > 0 ? `?${searchParams.toString()}` : ''}`
+
+	return useQuery<ProposalsWithCounts>({
 		...options,
 		queryKey: [url],
 		queryFn: async () => {
